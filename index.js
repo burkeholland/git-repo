30bac984c0e633e08d1465bd4a6cabf9d34c1ffa
Burke Holland <burkeholland@gmail.com>
2022-11-16 12:21:16 -0600

    update app

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 const { readFile, writeFileSync } = require("fs");
-const { promisify } = require('util');
-
-const paths = process.argv.slice(2);
-main(paths); // some comment
-
-async function main(paths) {
-    // print the welcome message
-    printWelcome();
-
-    // Compute the number of lines in each file
-    const data = await getLineCountInfo(paths);
-    console.log("Total Line Count: " + data.totalLineCount);
-    writeFileSync('output.json', JSON.stringify(data, null, 2));
-}
-
-/**
- * Prints the welcome message
-*/
-function printWelcome() {
-    console.log("Welcome To Line Counter");
-}
+const { promisify } = require("util");
 
 /*
  * A sample method
@@ -30,29 +10,4 @@ function sampleMethod(param1, param2, param3) {
 
     console.log("All params are now equal");
     console.log(`param1: ${param1}, param2: ${param2}, param3: ${param3}`);
-
-    // end of method
-}
-
-/**
- * @param {string[]} paths
-*/
-async function getLineCountInfo(paths) {
-    const lineCounts = await Promise.all(
-        paths.map(async path => ({
-            path,
-            count: getLineCount(await promisify(readFile)(path, 'utf8')) }
-        ))
-    );
-    return {
-        totalLineCount: lineCounts.reduce((acc, { count }) => acc + count, 0),
-        lineCounts,
-    };
-}
-
-/**
- * @param {string} str 
- */
-function getLineCount(str) {
-    return str.split('\n').length;
 }
