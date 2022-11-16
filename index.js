da503beb778a393b5c3941d5d2ecd652d916b208
Burke Holland <burkeholland@gmail.com>
2022-11-16 12:29:13 -0600

    update app

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const { readFile, writeFileSync } = require("fs");
-const { promisify } = require("util");
+const { readFile, writeFileSync } = require(`fs`);
+const { promisify } = require(`util`);
 
 /*
  * A sample method
 */
 function sampleMethod(param1, param2, param3) {
     param1 = param3;
+    console.log('Setting params equal to each other');
     param2 = param1;
 
-    console.log("All params are now equal");
-    console.log(`param1: ${param1}, param2: ${param2}, param3: ${param3}`);
+    console.log('param1: %s, param2: %s, param3: %s', param1, param2, param3);
 }
